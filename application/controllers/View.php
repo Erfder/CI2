9dae319051c329459f7ad0a89c7b21c133c4f97a
Erfder <caherman@fullsail.edu>
2016-02-16 21:32:56 -0700

    moving return info to controller

--- application/controllers/View.php
+++ application/controllers/View.php
@@ -10,8 +10,11 @@ class View extends CI_Controller {
 
 	public function index(){
 		$this->load->model('model3');
+		$rand = $this->model3->getrando();
+        $data['userinf'] = $rand;
+
 		$this->load->view('header');
-		$this->load->view('view1');
+		$this->load->view('view1', $data);
 		$this->load->view('footer'); 
 	}
 }
