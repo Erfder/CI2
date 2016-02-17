8b0d8c2fdce943fd794e54a9e3feb137b3cc70ff
Erfder <caherman@fullsail.edu>
2016-02-16 21:57:05 -0700

    ironed out randomization

--- application/models/model3.php
+++ application/models/model3.php
@@ -7,10 +7,10 @@ class Model3 extends CI_Model {
   		$this->db->select('userid');
 		$this->db->order_by("userid", "desc");
 		$this->db->limit(1);
-		$maxnu = $this->db->get('users');
-
-		$intmax = intval($maxnu);
-		$randomiz = rand(1, $intmax);
+		$maxnu = $this->db->get('users')->row_array();
+		//$intmax = settype($maxnu['userid'], "int");
+		//return $intmax;
+		$randomiz = rand(1,$maxnu['userid']);
 
 		$query = $this->db->get_where('users', array('userid' => $randomiz));
 		return $query->row_array();
