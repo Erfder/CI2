247914cdb4cd6a23627eef1e93a697c6ec22d82d
Erfder <caherman@fullsail.edu>
2016-02-24 17:09:19 -0700

    now the link is working as well

--- application/views/account.php
+++ application/views/account.php
@@ -1,14 +1,19 @@
 <div id="brown">
-<h1><?php echo $_SESSION['session_data']['Username']." ";  
-	 $this->load->helper('form');
-?></h1>
+<h1><?php echo $_SESSION['session_data']['Username']." "; $this->load->helper('form'); ?></h1>
 
 <?php 
-	echo form_open('User');
+	echo form_open('user');
+	$data= array(
+		'name' => 'u_name',
+		'required' => 'required',
+		'value' => 'jill',
+		//'style' => 'display:none;'
+	);
+	echo form_input($data);
 	$data = array(
-		'u_name' => '5',
+		'name' => 'sub',
 		'type' => 'submit',
-		'value'=> 'Jill',
+		'value'=> 'Go to jill',
 		'class'=> 'button'
 	);
 	echo form_submit($data); 
@@ -17,7 +22,7 @@
 
 
 <p>Here is your account. At this time you may upload new images or update your past images.</p>
-	<?php
+	<?php $this->load->helper('form');
 echo form_open_multipart('Control'); 
 
 	echo form_label('Choose an image from your computer', 'asdf')."<br>";
